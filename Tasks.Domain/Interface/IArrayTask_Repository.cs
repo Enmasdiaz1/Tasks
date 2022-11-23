b372fa7acda520122fcb0deaf3bf9a430780bb4b
Enmasdiaz <enmasd02@gmail.com>
2022-11-23 15:27:05 -0400

    Backend realizado, falta construir vista

---
+++ Tasks.Domain/Interface/IArrayTask_Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tasks.Domain.Models;
+
+namespace Tasks.Domain.Interface
+{
+    public interface IArrayTask_Repository
+    {
+        ArrayModel_Domain ReverseIt(string parametros);
+    }
+}
---
+++ Tasks.Web/Controllers/ArrayTask.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tasks.Application.Services.ArrayTask.Interface;
+
+namespace Tasks.Web.Controllers
+{
+    public class ArrayTask : Controller
+    {
+        private readonly IArrayTask_Sevices arrayTask_Sevices;
+        public ArrayTask(IArrayTask_Sevices arrayTask_Sevices)
+        {
+            this.arrayTask_Sevices = arrayTask_Sevices;
+        }
+        public IActionResult Index(string caracteres)
+        {
+            var result= arrayTask_Sevices.ReverseIt(caracteres);
+            return View(result);
+        }
+    }
+}
